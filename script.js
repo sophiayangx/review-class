3725af88eacdec0e5b9a582aa0e9ef27db64421f
sophiayangx <sopyang@nuevaschool.org>
2016-10-14 09:51:37 -0700

    Classadoo autocommit

---
+++ script.js
@@ -0,0 +1,38 @@
+//function(arg 1)
+var greeting = function(name) {
+console.log("Hi my name is" + " " + name);
+};
+
+greeting("Sophia");
+
+//jquery function
+$("#button").click(function() {
+  console.log("hihi");
+});
+
+
+//function(arg1, arg2)
+var perimeter = function(length, width) 
+{
+  console.log(length + length + width + width);
+};
+
+perimeter(3,5);
+
+//if statement
+if (4%2===0) {
+console.log("noice");
+}
+  
+else {
+console.log("darn");
+};
+
+//for loops
+for (var i = 1; i < 11; i = i + 1) {
+  console.log(i);
+}
+
+//list
+var list = [1,2,3,4]
+console.log(list);
